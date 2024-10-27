0e24a01a2afdb6ba1b2a9dc0ee0b4341c703ad97
fadilaahmads <fadilahmad59@gmail.com>
2024-10-27 23:04:58 +0700

    add db module import. change db query to use local postgres

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,12 +1,12 @@
  import bcrypt from 'bcrypt';
- import { db } from '../../db';
+ import { connectionPool } from '../../db';
  import { invoices, customers, revenue, users } from '../lib/placeholder-data'; 
 
- const client = await db.connect();
+ //const client = await db.connect();
 
  async function seedUsers() {
-   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-   await client.sql`
+   await connectionPool.query`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+   await connectionPool.query`
      CREATE TABLE IF NOT EXISTS users (
        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
        name VARCHAR(255) NOT NULL,
@@ -18,7 +18,7 @@
    const insertedUsers = await Promise.all(
      users.map(async (user) => {
        const hashedPassword = await bcrypt.hash(user.password, 10);
-       return client.sql`
+       return connectionPool.query`
          INSERT INTO users (id, name, email, password)
          VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
          ON CONFLICT (id) DO NOTHING;
@@ -30,9 +30,9 @@
  }
 
  async function seedInvoices() {
-   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+   await connectionPool.query`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-   await client.sql`
+   await connectionPool.query`
      CREATE TABLE IF NOT EXISTS invoices (
        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
        customer_id UUID NOT NULL,
@@ -44,7 +44,7 @@
 
    const insertedInvoices = await Promise.all(
      invoices.map(
-       (invoice) => client.sql`
+       (invoice) => connectionPool.query`
          INSERT INTO invoices (customer_id, amount, status, date)
          VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
          ON CONFLICT (id) DO NOTHING;
@@ -56,9 +56,9 @@
  }
 
  async function seedCustomers() {
-   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+   await connectionPool.query`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-   await client.sql`
+   await connectionPool.query`
      CREATE TABLE IF NOT EXISTS customers (
        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
        name VARCHAR(255) NOT NULL,
@@ -69,7 +69,7 @@
 
    const insertedCustomers = await Promise.all(
      customers.map(
-       (customer) => client.sql`
+       (customer) => connectionPool.query`
          INSERT INTO customers (id, name, email, image_url)
          VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
          ON CONFLICT (id) DO NOTHING;
@@ -81,7 +81,7 @@
  }
 
  async function seedRevenue() {
-   await client.sql`
+   await connectionPool.query`
      CREATE TABLE IF NOT EXISTS revenue (
        month VARCHAR(4) NOT NULL UNIQUE,
        revenue INT NOT NULL
@@ -90,7 +90,7 @@
 
    const insertedRevenue = await Promise.all(
      revenue.map(
-       (rev) => client.sql`
+       (rev) => connectionPool.query`
          INSERT INTO revenue (month, revenue)
          VALUES (${rev.month}, ${rev.revenue})
          ON CONFLICT (month) DO NOTHING;
@@ -102,21 +102,21 @@
  }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  //return Response.json({
+  //  message:
+  //    'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  //});
    try {
-     await client.sql`BEGIN`;
+     //await connectionPool.query`BEGIN`;
      await seedUsers();
      await seedCustomers();
      await seedInvoices();
      await seedRevenue();
-     await client.sql`COMMIT`;
+     //await connectionPool.query`COMMIT`;
 
      return Response.json({ message: 'Database seeded successfully' });
    } catch (error) {
-     await client.sql`ROLLBACK`;
+     //await connectionPool.query`ROLLBACK`;
      return Response.json({ error }, { status: 500 });
    }
 }
